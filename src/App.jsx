bf3dca033a7b6050e0bc78ab25e053802351ebcc
Shahbaz Ansari <116244599+Shahbaz9832@users.noreply.github.com>
2026-07-27 12:50:58 +0530

    Uncomment Resume import in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-// import Resume from "./components/Resume";
+import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
